dd8e0b601cb2915506faa50b4bee6b3121a5720b
Vaishnav Verma <vermavaishnav70@gmail.com>
2024-12-06 01:15:49 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,8 @@
+const http = require('http')
+const app = require('./app')
+const PORT = process.env.PORT
+const server = http.createServer(app)
+
+server.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
